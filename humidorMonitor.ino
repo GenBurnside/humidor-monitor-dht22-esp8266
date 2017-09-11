7e36c78a624c6b9e613be441c7e928ba80b7ee50
Matt Hughes <matthughes.tech@gmail.com>
2017-09-10 23:39:21 -0400

    Publish temperature and humidity readings to Adafruit IO

--- humidorMonitor.ino
+++ humidorMonitor.ino
@@ -1,20 +1,25 @@
 #include <DHT.h>
-#include <ESP8266WiFi.h>
+#include <AdafruitIO_WiFi.h>
 
 DHT dht(2, DHT22);
 
+char ioUser[] = "";
+char ioKey[] = "";
 char ssid[] = "";
 char password[] = "";
+
+AdafruitIO_WiFi io(ioUser, ioKey, ssid, password);
+AdafruitIO_Feed *humFeed = io.feed("humidor-humidity");
+AdafruitIO_Feed *tempFeed = io.feed("humidor-temperature");
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  connectToWifi();
 }
 
 void loop() {
-  if (WiFi.status() != WL_CONNECTED) {
-    connectToWifi();
-  }
+  io.run();
 
   float hum = dht.readHumidity();
   float temp = dht.readTemperature(true);
@@ -30,22 +35,30 @@ void loop() {
   Serial.print(temp);
   Serial.print(" *F\t");
   Serial.println();
-    
-  delay(5000);
+
+  humFeed->save(hum);
+  tempFeed->save(temp);
+
+  delay(10000);
 }
 
 void connectToWifi() {
   Serial.println();
   Serial.print("Connecting to network: ");
   Serial.print(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+
+  io.connect();
+
+  while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
   }
-  Serial.println("");
+
+  Serial.println();
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  IPAddress ip = WiFi.localIP();
-  Serial.println(ip);
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.println();
+
+  Serial.println(io.statusText());
 }
